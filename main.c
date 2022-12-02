5fa0d73196c3dc813c23baed30a7d82d875793f8
Daniel-tech-cloud <guardians01@outlook.com>
2022-12-02 12:49:49 -0600

    Solucioné un bug de la función reflejar

--- main.c
+++ main.c
@@ -3,40 +3,87 @@
 
 int origenX = 0;
 int origenH = 1;
+int suma = 0, max = 0;   
 
-void reflejar (int h[], size_t tamano) {
-    int aux = 0;
+void reflejar (int h[], size_t tamano);
+int* multiplicar (int h[], size_t tamanoH, int x[], size_t tamanoX);
+void desplazar(int n);
+void sumar(int *producto, int longitud;);
 
-    printf("Longitud de h: %d\n", tamano);
 
-    for(int i=0; i<=tamano/2; i++) {
-        tamano--;
-        aux = h[i];
-        h[i] = h[tamano]; 
-        h[tamano] = aux;
+int main () {
+
+    int x [] = {1, 2, 1, -1};
+    int h [] = {1, 2, 3, 1}; //invertir: 0 0 1 2 3 1
+	int* multiplicacion;
+	
+	//Funci�n x
+	printf("Longitud de x: %d\n", sizeof(x)/sizeof(x[0]));
+	for(int i=0; i<sizeof(x)/sizeof(x[0]); i++){
+        printf("%d, ", x[i]);
     }
+    
+    // Funci�n h
+	printf("\n");
+    printf("Longitud de h: %d\n", sizeof(h)/sizeof(h[0]));
+    for(int i=0; i<sizeof(h)/sizeof(h[0]); i++){
+        printf("%d, ", h[i]);
+    }
+    printf("\n");
+    
+	// Reflexi�n 	
+    reflejar(h, sizeof(h)/sizeof(h[0]));    
+    printf("\n");
+    
+    // Desplazamiento
+    desplazar(0);
+    
+    // Multiplicaci�n
+    printf("\n");
+    printf("\n");
+    printf("\n");
+	multiplicacion = multiplicar(h, sizeof(h)/sizeof(h[0]), x, sizeof(x)/sizeof(x[0]));
+    sumar(multiplicacion, max);
 
-    origenH = -origenH;
+    return 0;
 }
 
-/**/
+// Reflejar
+void reflejar (int h[], size_t tamano) {
+    
+	int aux = 0;
+  	for (int i = 0; i < tamano / 2; i++) {	    
+	    aux = h[i];	    
+	    h[i] = h[tamano - i - 1];
+    	h[tamano - i - 1] = aux;
+  	}
+  	
+  	printf("Reflexion h: %d\n", tamano);
+  	for(int i = 0; i< tamano; i++)
+  	{
+  		printf("%d, ", h[i]);
+	}
+  	
+	origenH = -origenH;
+}
 
+// Desplazar
 void desplazar(int n) {                 
-    //Desplazar a la derecha
-    if(n = 1){
-        origenH--;
-    }
     //Desplazar a la izquierda
-    else if(n == -1){
+    if(n > 0){
         origenH++;
     }
+    //Desplazar a la derecha
+    else if(n < 0){
+        origenH--;
+    }
 }
 
-
+// Multiplicar
 int* multiplicar (int h[], size_t tamanoH, int x[], size_t tamanoX) {
 
-    //Se define el tamaño máximo
-    int max = tamanoH > tamanoX ? tamanoH : tamanoX;
+    //Se define el tama�o m�ximo
+    max = tamanoH > tamanoX ? tamanoH : tamanoX;
 
     //Arreglo nuevo
     int* producto = (int*)malloc(sizeof(int) * max);
@@ -48,48 +95,21 @@ int* multiplicar (int h[], size_t tamanoH, int x[], size_t tamanoX) {
             producto[i] = h[i] * x[i];
     }
 
-
+	printf("Multiplicaci�n de h * x: \n");
     for(int i=0; i<max; i++){
-        printf("%d, ", producto[i]);
+        printf("%d,", producto[i]);        
     }
 
     return producto;
 }
 
+// Sumar
+void sumar(int* producto, int longitud){  
 
-int main () {
-
-    int x [] = {1, 2, 3, 1};
-    int h [] = {1, 2, 3, 4, 5, 6, 7};
-
-    printf("Longitud de h: %d\n", sizeof(h)/sizeof(h[0]));
-
-    for(int i=0; i<sizeof(h)/sizeof(h[0]); i++){
-        printf("%d, ", h[i]);
+    for(int i = 0; i < longitud; i++){
+        suma += producto[i];
     }
-
     printf("\n");
-    printf("\n");
-    printf("\n");
+    printf("Suma: %i ", suma);
 
-    reflejar(h, sizeof(h)/sizeof(h[0])) ;
-
-    for(int i=0; i<sizeof(h)/sizeof(h[0]); i++){
-        printf("%d, ", h[i]);
-    }
-
-
-    //Multiplicación
-    printf("\n");
-    printf("\n");
-    printf("\n");
-
-    multiplicar(h, sizeof(h)/sizeof(h[0]), x, sizeof(x)/sizeof(x[0]));
-
-
-    // for(int i=0; i<sizeof(producto)/sizeof(producto[0]); i++){
-    //     printf("%d, ", producto[i]);
-    // }
-
-    return 0;
 }
